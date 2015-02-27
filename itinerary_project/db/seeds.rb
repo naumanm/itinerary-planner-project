f67cb5b1bf151d5b9135de0e7d09e4d8286bed4e
michael nauman <bodhiandmike@gmail.com>
2015-02-27 15:06:19 -0800

    added spaces

--- itinerary_project/db/seeds.rb
+++ itinerary_project/db/seeds.rb
@@ -5,6 +5,8 @@ riyadh = Destination.create(place: "Riaydh, Saudi Arabia")
 tokyo = Destination.create(place: "Tokyo, JP")
 
 honeymoon = Itinerary.create(name: "honeymoon", itin_s_date: '01/01/14', itin_r_date: '02/03/14')
+
 mikesbigtrip = Itinerary.create(name: "mikes big trip", itin_s_date: '01/01/14', itin_r_date: '02/03/14')
+
 travelers = Traveler.create(first_name: 'Paris', last_name: 'Jones', email: 'Abraham@ga.com')
 
