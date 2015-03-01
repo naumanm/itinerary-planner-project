8372bee7624bf246151ec2c422a950c916f59f2c
Abraham Hall <abrahamhall84@gmail.com>
2015-03-01 14:09:39 -0800

    updated routes

--- itinerary_project/config/routes.rb
+++ itinerary_project/config/routes.rb
@@ -3,17 +3,11 @@ Rails.application.routes.draw do
 root 'index#hompage'
 
 resources :itineraries do 
-  res
-  resources :itineraries, :has_many => :comments
+  resources :travelers
+  resources :destinations
+end
 
-  root 'access#login'
+resources :itineraries, :has_many => :comments
 
-  get '/login', to: 'access#login', as: 'login'
-
-  get '/signup', to: 'access#signup', as: 'signup'
-
-  get '/main', to: 'access#main', as: 'main'
-
-  post '/', to: 'authors#create'
 
 end
