84d02b78fd7ed3d490614132142fd0cb641304eb
michael nauman <bodhiandmike@gmail.com>
2015-03-01 16:05:36 -0800

    updated destinations

--- itinerary_project/app/controllers/destinations_controller.rb
+++ itinerary_project/app/controllers/destinations_controller.rb
@@ -1,10 +1,24 @@
 class DestinationsController < ApplicationController
+  before_action :set_destination, only: [:show, :edit]
+
   def edit
   end
 
   def new
+    @destination = Destination.new
   end
 
   def show
+    @destinations = Destination.all
   end
+
+  private
+    def set_destination
+      @destination = Destination.find_by_id(params[:id])
+    end
+
+    def destination_params
+      params.require(:destination).permit(:place, :start_date, :return_date)
+    end
+
 end
